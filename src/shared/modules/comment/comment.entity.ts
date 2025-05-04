8963bc4eedda965d358fb9a3b7db28ed57ea20f9
kuryuo <kutabare.wa@gmail.com>
2025-05-05 03:00:47 +0500

    feat: module5-task2

---
+++ src/shared/modules/comment/comment.entity.ts
@@ -0,0 +1,33 @@
+import {
+  defaultClasses,
+  getModelForClass,
+  modelOptions,
+  prop,
+  Ref,
+} from '@typegoose/typegoose';
+import { OfferEntity } from '../offer/index.js';
+import { UserEntity } from '../user/index.js';
+
+export interface CommentEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'comments',
+    timestamps: true,
+  },
+})
+export class CommentEntity extends defaultClasses.TimeStamps {
+  @prop({ required: true, trim: true })
+  public text!: string;
+
+  @prop({ required: true })
+  public rating!: number;
+
+  @prop({ ref: () => OfferEntity, required: true })
+  public offerId!: Ref<OfferEntity>;
+
+  @prop({ ref: () => UserEntity, required: true })
+  public userId!: Ref<UserEntity>;
+}
+
+export const CommentModel = getModelForClass(CommentEntity);
